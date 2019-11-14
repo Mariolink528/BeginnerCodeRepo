e79f5c8c02ff9b3d3ed43aae4b20dae01f565f8a
Mariolink528 <Tyler.Thraves@yahoo.com>
2019-11-14 13:52:07 -0800

    Randomizing Potion Locations

--- BeginnerCode/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ BeginnerCode/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -5,23 +5,25 @@ public class SpawnerSample : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
 
+   
+   
     void Start()
     {
-        int angle = 15;
-        int distance = Random.Range(2, 15);
-
         Vector3 spawnPosition = transform.position;
+        SpawnPotion(spawnPosition, 2.0f, 15.0f);
+        SpawnPotion(spawnPosition, 5.0f, 15.0f);
+        SpawnPotion(spawnPosition, 2.0f, 8.0f);
+
+    }
+    // Let's set 3 parameters (minDistance and maxDistance)
+    void SpawnPotion(Vector3 spawnPosition, float minDistance, float maxDistance)
+    {
+        int angle = Random.Range(0, 360);
+
+        float distance = Random.Range( minDistance, maxDistance);
+
+        
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * distance;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * distance;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
         spawnPosition = transform.position + direction * distance;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
