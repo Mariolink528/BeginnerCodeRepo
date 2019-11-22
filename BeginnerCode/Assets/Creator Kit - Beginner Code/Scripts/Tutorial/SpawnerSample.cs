c89f392296ba7576bc68c58aa4f3dcd4a7373191
Mariolink528 <Tyler.Thraves@yahoo.com>
2019-11-22 13:23:13 -0800

    Making potions spawned adjustable

--- BeginnerCode/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ BeginnerCode/Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -4,21 +4,22 @@ using CreatorKitCode;
 public class SpawnerSample : MonoBehaviour
 {
     public GameObject ObjectToSpawn;
+    public int numberOfSpawns;
+    Vector3 spawnPosition;
 
-   
-   
     void Start()
     {
-        
-        SpawnPotion(2.0f, 15.0f);
-        SpawnPotion(5.0f, 15.0f);
-        SpawnPotion(2.0f, 8.0f);
+        spawnPosition = transform.position;
+        for (int i = 0; i < numberOfSpawns; i++)
+        {
+            SpawnPotion(2.0f, 15.0f);
+        }
 
     }
     // Let's set 2 parameters (minDistance and maxDistance)
     void SpawnPotion(float minDistance, float maxDistance)
     {
-        Vector3 spawnPosition = transform.position;
+       
 
         int angle = Random.Range(0, 360);
 
